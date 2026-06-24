a3b61a0ce52cf6088cbcc7417622cbf52a3829fa
s.moharrarzadeh <sabamoharrerzade@gmail.com>
2026-06-25 00:19:42 +0330

    feat: implement planner agent with Pydantic-based structured search query output

---
+++ planner_agent.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from agents import Agent
+
+HOW_MANY_SEARCHES = 5
+
+INSTRUCTIONS = f"You are a helpful research assistant. Given a query, come up with a set of web searches \
+to perform to best answer the query. Output {HOW_MANY_SEARCHES} terms to query for."
+
+
+class WebSearchItem(BaseModel):
+    reason: str
+    "Your reasoning for why this search is important to the query"
+
+    query: str
+    "The search term to use for the web search."
+
+class WebSearchPlan(BaseModel):
+    """
+    Represents the output of the planning agent, containing the list of targeted web searches to be performed.
+    """
+    searches: list[WebSearchItem] 
+
+planner_agent = Agent(
+    name= "Planner Agent",
+    instructions= INSTRUCTIONS,
+    model="openai/gpt-oss-120b:free",
+    output=WebSearchPlan
+)
